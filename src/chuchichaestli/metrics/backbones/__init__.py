496854f729677f401183f242323f9edd27622edd
danielbarco <danielaabarco@gmail.com>
2024-11-28 15:06:35 +0100

    rmv torchmetrics

---
+++ src/chuchichaestli/metrics/backbones/__init__.py
@@ -0,0 +1,10 @@
+from .inception import InceptionEncoder
+from .swav import ResNet50Encoder #, ResNet18Encoder
+from .mae import VisionTransformerEncoder
+from .data2vec import HuggingFaceTransformerEncoder
+from .clip import CLIPEncoder
+from .convnext import ConvNeXTEncoder
+from .dinov2 import DINOv2Encoder
+from .load_encoder import load_encoder, MODELS
+from .simclr import SimCLRResNetEncoder
+from .resizer import pil_resize
--- src/chuchichaestli/metrics/fd.py
+++ src/chuchichaestli/metrics/fd.py
@@ -1,22 +1,16 @@
 import torch
 from torchmetrics import Metric
-from chuchichaestli.metrics.fd import (
-    compute_statistics,
-    compute_FD_with_reps,
-    compute_efficient_FD_with_reps,
-)
 import numpy as np
 from scipy import linalg
 from tqdm import tqdm
 from sklearn.linear_model import LinearRegression
 
-from representations import get_representation
-from .models import load_encoder
+from .backbones.load_encoder import load_encoder
 
 
 class FD(Metric):
     
-    def __init__(self, model_name: str, device: str):
+    def __init__(self, model_name: str, device_str: str):
         # remember to call super
         super().__init__()
         # call `self.add_state`for every internal state that is needed for the metrics computations
@@ -24,14 +18,51 @@ class FD(Metric):
         # state from multiple processes
         self.add_state("real", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("fake", default=torch.tensor(0), dist_reduce_fx="sum")
-
-        self.model = load_encoder(model_name, device, ckpt=None, arch=None,
+        
+        self.model = load_encoder(model_name, device_str, ckpt=None, arch=None,
                     clean_resize=False, #Use clean resizing (from pillow)
                     sinception=True if model_name=='sinception' else False,
                     depth=0, # Negative depth for internal layers, positive 1 for after projection head.
-                    )
-        self.device = device
+                    )        
+        
+    def get_representation(self, model, batch, device, normalized=False):
+        if isinstance(batch, list):
+            # batch is likely list[array(images), array(labels)]
+            batch = batch[0]
 
+        if not torch.is_tensor(batch):
+            # assume batch is then e.g. AutoImageProcessor.from_pretrained("facebook/data2vec-vision-base")
+            batch = batch["pixel_values"]
+            batch = batch[:, 0]
+
+        # Convert grayscale to RGB
+        if batch.ndim == 3:
+            batch.unsqueeze_(1)
+        if batch.shape[1] == 1:
+            batch = batch.repeat(1, 3, 1, 1)
+
+        batch = batch.to(device)
+
+        with torch.no_grad():
+            pred = model(batch)
+
+            if not torch.is_tensor(pred):  # Some encoders output tuples or lists
+                pred = pred[0]
+
+        # If model output is not scalar, apply global spatial average pooling.
+        # This happens if you choose a dimensionality not equal 2048.
+        if pred.dim() > 2:
+            if pred.size(2) != 1 or pred.size(3) != 1:
+                pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+            pred = pred.squeeze(3).squeeze(2)
+
+        if normalized:
+            pred = torch.nn.functional.normalize(pred, dim=-1)
+        pred = pred.cpu().numpy()
+        
+        return pred
+    
     def compute_FD_with_stats(self, mu1, mu2, sigma1, sigma2, eps=1e-6):
         """
         Numpy implementation of the Frechet Distance.
@@ -88,7 +119,7 @@ class FD(Metric):
 
     def update(self, batch, real:bool) -> None:
         
-        features = get_representation(self.model, batch, self.device, normalized=False)
+        features = self.get_representation(self.model, batch, self.device, normalized=False)
         # extract predicted class index for computing accuracy
         if real: 
             self.real += features.sum(dim=0)
