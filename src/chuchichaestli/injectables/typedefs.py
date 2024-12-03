3418f89e69f5d6c12a4bf873108838c873ad17f2
Martin Oswald <73196164+oswdm@users.noreply.github.com>
2024-12-03 17:11:09 +0100

    adds typedefinitions for injectibles

---
+++ src/chuchichaestli/injectables/typedefs.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, Any
+from torch import Tensor
+from typing import Protocol
+
+# --------------------------------------------------------------------------------------
+# types
+# --------------------------------------------------------------------------------------
+class STEP_OUTPUT(TypedDict):
+    """A typed dictionary representing the output of a training or validation step.
+
+    Attributes:
+        loss (Tensor): The loss value for the step.
+        inputs (Tensor): The input data for the step.
+        output (Tensor): The output of the model for the step.
+        target (Tensor): The target data for the step.
+    """
+
+    loss: Tensor
+    inputs: Tensor
+    output: Tensor
+    target: Tensor
+
+# --------------------------------------------------------------------------------------
+# protocols
+# --------------------------------------------------------------------------------------
+
+class Fätch(Protocol):
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        ...
