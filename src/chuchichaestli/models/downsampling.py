281e55214609feadcc08171e10f6286ff10f3b46
phdenzel <phdenzel@gmail.com>
2025-11-05 22:59:11 +0100

    Added flexible pooling layer module

--- src/chuchichaestli/models/downsampling.py
+++ src/chuchichaestli/models/downsampling.py
@@ -6,22 +6,36 @@
 import torch
 from torch.nn import Module
 from torch.nn import functional as F
-from chuchichaestli.models.maps import DIM_TO_CONV_MAP, DOWNSAMPLE_MODE
+from chuchichaestli.models.maps import DIM_TO_CONV_MAP, DIM_TO_POOL_MAP, DOWNSAMPLE_MODE
+from chuchichaestli.utils import partialclass
 from typing import Literal
+
+
+__all__ = [
+    "Downsample",
+    "DownsampleInterpolate",
+    "Pool",
+    "MaxPool",
+    "AdaptiveMaxPool",
+    "AvgPool",
+    "AdaptiveAvgPool",
+    "DOWNSAMPLE_FUNCTIONS",
+]
 
 
 class Downsample(Module):
     """Downsampling layer for 1D, 2D, and 3D inputs."""
 
-    def __init__(self, dimensions: int, num_channels: int):
+    def __init__(self, dimensions: int, num_channels: int, **kwargs):
         """Initialize the downsampling layer."""
         super().__init__()
         conv_cls = DIM_TO_CONV_MAP[dimensions]
-        self.conv = conv_cls(
-            num_channels, num_channels, kernel_size=3, stride=2, padding=1
-        )
+        kwargs.setdefault("kernel_size", 3)
+        kwargs.setdefault("stride", 2)
+        kwargs.setdefault("padding", 1)
+        self.conv = conv_cls(num_channels, num_channels, **kwargs)
 
-    def forward(self, x: torch.Tensor, _t: torch.Tensor | None = None) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, *args) -> torch.Tensor:
         """Forward pass through the downsampling layer."""
         return self.conv(x)
 
@@ -38,6 +52,7 @@ class DownsampleInterpolate(Module):
         num_channels: int | None = None,
         factor: int | None = None,
         antialias: bool = False,
+        **kwargs,
     ):
         """Initialize the downsampling layer."""
         super().__init__()
@@ -52,7 +67,7 @@ class DownsampleInterpolate(Module):
         """Interpolation mode."""
         return DOWNSAMPLE_MODE.get(self.dimensions, "nearest")
 
-    def forward(self, x: torch.Tensor, _t: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, *args) -> torch.Tensor:
         """Forward pass through the downsampling layer."""
         spatial_dims = x.shape[2:]
         output_dims = [s // self.factor for s in spatial_dims]
@@ -65,7 +80,48 @@ class DownsampleInterpolate(Module):
         )
 
 
+class Pool(Module):
+    """Max/avg (optionally adaptive) pooling layer for 1D, 2D, and 3D inputs."""
+
+    def __init__(
+        self,
+        dimensions: int,
+        num_channels: int | None = None,
+        average: bool = False,
+        adaptive: bool = False,
+        **kwargs,
+    ):
+        """Initialize the pooling layer (default: max pooling)."""
+        super().__init__()
+        pool_type = "MaxPool"
+        kwargs.setdefault("kernel_size", 3)
+        kwargs.setdefault("stride", 2)
+        kwargs.setdefault("padding", 1)
+        if average:
+            pool_type = "AvgPool"
+        if adaptive:
+            pool_type = "Adaptive" + pool_type
+            out = kwargs.get("output_size", None)
+            kwargs = {"output_size": out if out is not None else (1,)*dimensions}
+        pool_cls = DIM_TO_POOL_MAP[dimensions][pool_type]
+        self.pool = pool_cls(**kwargs)
+
+    def forward(self, x: torch.Tensor, *args) -> torch.Tensor:
+        """Forward pass through the pooling layer."""
+        return self.pool(x)
+
+
+MaxPool = partialclass("MaxPool", Pool, average=False, adaptive=False)
+AdaptiveMaxPool = partialclass("AdaptiveMaxPool", Pool, adaptive=True)
+AvgPool = partialclass("AvgPool", Pool, average=True)
+AdaptiveAvgPool = partialclass("AdaptiveAvgPool", Pool, average=True, adaptive=True)
+
+
 DOWNSAMPLE_FUNCTIONS = {
     "Downsample": Downsample,
     "DownsampleInterpolate": DownsampleInterpolate,
+    "MaxPool": MaxPool,
+    "AdaptiveMaxPool": AdaptiveMaxPool,
+    "AvgPool": AvgPool,
+    "AdaptiveAvgPool": AdaptiveAvgPool,
 }
