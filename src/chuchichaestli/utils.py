11465275e81e27e650e7263af3e130aa62135543
phdenzel <phdenzel@gmail.com>
2025-11-05 23:00:11 +0100

    Keyword aliasing decorator + docstrings for partialclass

--- src/chuchichaestli/utils.py
+++ src/chuchichaestli/utils.py
@@ -4,11 +4,13 @@
 """Various utility functions for chuchichaestli."""
 
 import sys
-from functools import partialmethod
+from functools import partialmethod, wraps
+from collections.abc import Callable
 
 
 def partialclass(name: str, cls: type[object], *args, **kwargs):
     """Partial for __init__ class constructors."""
+    docstring = kwargs.pop("__doc__", None)
     part_cls = type(
         name, (cls,), {"__init__": partialmethod(cls.__init__, *args, **kwargs)}
     )
@@ -16,4 +18,35 @@ def partialclass(name: str, cls: type[object], *args, **kwargs):
         part_cls.__module__ = sys._getframe(1).f_globals.get("__name__", "__main__")
     except (AttributeError, ValueError):
         pass
+    if docstring is not None:
+        part_cls.__doc__ = docstring
     return part_cls
+
+
+def alias_kwargs(key: str | dict[str, str], alias: str | None = None) -> Callable:
+    """Decorator for aliasing keyword arguments in a function.
+
+    Args:
+        key: Name of keyword argument in function to alias, or a dictionary mapping keywords to aliases.
+        alias: Alias that can be used for the specified keyword argument.
+    """
+    if alias is None and isinstance(key, dict):
+        alias = key.values()
+        key = key.keys()
+    if isinstance(key, str):
+        key = (key,)
+    if isinstance(alias, str):
+        alias = (alias,)
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for k, a in zip(key, alias):
+                alias_value = kwargs.get(a, None)
+                if alias_value is not None:
+                    kwargs[k] = alias_value
+                if a in kwargs:
+                    del kwargs[a]
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
