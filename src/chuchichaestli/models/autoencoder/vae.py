f275192e41d56f73f7ca7b2babc60dfb57661860
phdenzel <phdenzel@gmail.com>
2025-11-07 23:29:44 +0100

    Updated VAE module

--- src/chuchichaestli/models/autoencoder/vae.py
+++ src/chuchichaestli/models/autoencoder/vae.py
@@ -1,108 +1,166 @@
-"""Variational autoencoder implementation.
-
-This file is part of Chuchichaestli.
-
-Chuchichaestli is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Chuchichaestli is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Chuchichaestli.  If not, see <http://www.gnu.org/licenses/>.
-
-Developed by the Intelligent Vision Systems Group at ZHAW.
-"""
+# SPDX-FileCopyrightText: 2024-present Members of CAIIVS
+# SPDX-FileNotice: Part of chuchichaestli
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""A highly-customizable variational autoencoder implementation."""
 
 import torch
 from torch import nn
-
-from chuchichaestli.models.vae.decoder import Decoder
-from chuchichaestli.models.vae.encoder import Encoder
+from torch.distributions import MultivariateNormal, kl
+from chuchichaestli.models.activations import ActivationTypes
+from chuchichaestli.models.autoencoder.decoder import Decoder
+from chuchichaestli.models.autoencoder.encoder import Encoder
+from chuchichaestli.models.blocks import (
+    AutoencoderDownBlockTypes,
+    AutoencoderMidBlockTypes,
+    AutoencoderUpBlockTypes,
+    EncoderOutBlockTypes,
+    DecoderInBlockTypes,
+)
+from chuchichaestli.models.maps import DIM_TO_CONV_MAP
+from chuchichaestli.models.norm import NormTypes
+from chuchichaestli.utils import prod
+from typing import Literal
+from collections.abc import Sequence
 
 
 class VAE(nn.Module):
-    """Variational autoencoder implementation."""
+    """Flexible variational autoencoder implementation.
+
+    The architecture consists of an encoder-decoder structure.
+    The encoder chains several residual and downsampling blocks.
+    Each downsampling block separates the encoder into spatially hierarchical levels.
+    The encoder ends in bottleneck blocks (optionally including attention blocks
+    and a convolutional layer) and projects the input into latent space.
+    Latent mean and variance are sampled from a Gaussian and passed to the decoder.
+    The decoder is built with residual convolutional and upsampling blocks, and
+    expands from the latent space to the image domain.
+    """
 
     def __init__(
         self,
         dimensions: int = 2,
-        in_channels: int = 3,
-        n_channels: int = 32,
+        in_channels: int = 1,
+        n_channels: int = 64,
         latent_dim: int = 4,
-        down_block_types: tuple[str] = ("EncoderDownBlock",),
-        mid_block_type: str = "EncoderMidBlock",
-        up_block_types: tuple[str] = ("EncoderUpBlock",),
-        encoder_out_block_type: str = "EncoderOutBlock",
-        decoder_in_block_type: str = "DecoderInBlock",
-        block_out_channel_mults: tuple[int] = (2,),
-        num_layers_per_block: int = 1,
-        res_groups: int = 4,
-        res_act_fn: str = "silu",
+        out_channels: int = 1,
+        down_block_types: Sequence[AutoencoderDownBlockTypes] = (
+            "AutoencoderDownBlock",
+            "AutoencoderDownBlock",
+            "AutoencoderDownBlock",
+            "AutoencoderDownBlock",
+        ),
+        down_layers_per_block: int = 2,
+        downsample_type: Literal["Downsample", "DownsampleInterpolate"] = "Downsample",
+        encoder_mid_block_types: Sequence[AutoencoderMidBlockTypes] = (
+            "AutoencoderMidBlock",
+            "AttnAutoencoderMidBlock",
+        ),
+        encoder_out_block_type: EncoderOutBlockTypes = "EncoderOutBlock",
+        decoder_in_block_type: DecoderInBlockTypes = "DecoderInBlock",
+        decoder_mid_block_types: Sequence[AutoencoderMidBlockTypes] = (
+            "AutoencoderMidBlock",
+            "AttnAutoencoderMidBlock",
+        ),
+        up_block_types: AutoencoderUpBlockTypes = (
+            "AutoencoderUpBlock",
+            "AutoencoderUpBlock",
+            "AutoencoderUpBlock",
+            "AutoencoderUpBlock",
+        ),
+        up_layers_per_block: int = 3,
+        upsample_type: Literal[
+            "Upsample", "UpsampleInterpolate"
+        ] = "UpsampleInterpolate",
+        block_out_channel_mults: Sequence[int] = (1, 2, 2, 2),
+        use_latent_proj: bool = True,
+        use_latent_deproj: bool = True,
+        res_act_fn: ActivationTypes = "silu",
         res_dropout: float = 0.1,
-        res_norm_type: str = "group",
+        res_norm_type: NormTypes = "group",
+        res_groups: int = 8,
         res_kernel_size: int = 3,
         attn_head_dim: int = 32,
         attn_n_heads: int = 1,
-        attn_gate_inter_channels: int = 32,
-        encoder_norm_num_groups: int = 4,
-        encoder_act_fn: str = "silu",
-        encoder_in_kernel_size: int = 3,
-        encoder_out_kernel_size: int = 3,
-        decoder_norm_num_groups: int = 4,
-        decoder_act_fn: str = "silu",
-        decoder_in_kernel_size: int = 3,
-        decoder_out_kernel_size: int = 3,
-        double_z: bool = True,
+        attn_dropout_p: float = 0.0,
+        attn_norm_type: NormTypes = "group",
+        attn_groups: int = 32,
+        attn_kernel_size: int = 1,
+        encoder_act_fn: ActivationTypes = "silu",
+        encoder_norm_type: NormTypes = "group",
+        encoder_groups: int = 8,
+        encoder_kernel_size: int = 3,
+        decoder_act_fn: ActivationTypes = "silu",
+        decoder_norm_type: NormTypes = "group",
+        decoder_groups: int = 8,
+        decoder_kernel_size: int = 3,
     ):
         """Initializes the VAE model with the given parameters.
 
         Args:
-            dimensions (int): Number of dimensions for the model. Default is 2.
-            in_channels (int): Number of input channels. Default is 3.
-            n_channels (int): Number of channels in the model. Default is 32.
-            latent_dim (int): Dimension of the latent space. Default is 4.
-            out_channels (int): Number of output channels. Default is 3.
-            down_block_types (tuple[str]): Types of downsampling blocks. Default is ("DownBlock",).
-            mid_block_type (str): Type of middle block. Default is "MidBlock".
-            up_block_types (tuple[str]): Types of upsampling blocks. Default is ("UpBlock",).
-            encoder_out_block_type (str): Type of output block for the encoder. Default is "EncoderOutBlock".
-            decoder_in_block_type (str): Type of input block for the decoder. Default is "DecoderInBlock".
-            block_out_channel_mults (tuple[int]): Multipliers for the number of output channels in each block. Default is (2,).
-            num_layers_per_block (int): Number of layers per block. Default is 1.
-            res_groups (int): Number of groups for the residual blocks. Default is 32.
-            res_act_fn (str): Activation function for the residual blocks. Default is "silu".
-            res_dropout (float): Dropout rate for the residual blocks. Default is 0.1.
-            res_norm_type (str): Normalization type for the residual blocks. Default is "group".
-            res_kernel_size (int): Kernel size for the residual blocks. Default is 3.
-            attn_head_dim (int): Dimension of each attention head. Default is 32.
-            attn_n_heads (int): Number of attention heads. Default is 1.
-            attn_gate_inter_channels (int): Number of intermediate channels for the attention gate. Default is 32.
-            encoder_norm_num_groups (int): Number of groups for normalization in the encoder. Default is 8.
-            encoder_act_fn (str): Activation function for the encoder. Default is "silu".
-            encoder_in_kernel_size (int): Kernel size for the input layer of the encoder. Default is 3.
-            encoder_out_kernel_size (int): Kernel size for the output layer of the encoder. Default is 3.
-            decoder_norm_num_groups (int): Number of groups for normalization in the decoder. Default is 8.
-            decoder_act_fn (str): Activation function for the decoder. Default is "silu".
-            decoder_in_kernel_size (int): Kernel size for the input layer of the decoder. Default is 3.
-            decoder_out_kernel_size (int): Kernel size for the output layer of the decoder. Default is 3.
-            double_z (bool): Whether to use double the latent space dimensions. Default is True.
+            dimensions: Number of dimensions for the model.
+            in_channels: Number of input channels.
+            n_channels: Number of channels in the hidden layer.
+            latent_dim: Number of channels in the latent space.
+            out_channels: Number of output channels.
+            down_block_types: Types of down block(s) to use for each level.
+            down_layers_per_block: Number of blocks per level in the encoder
+                (blocks are repeated if `>1`).
+            downsample_type: Type of downsampling block
+                (see `chuchichaestli.models.downsampling` for details).
+            encoder_mid_block_types: Types of middle block(s) in the encoder.
+            encoder_out_block_type: Type of output block in the encoder.
+            decoder_in_block_type: Type of input block in the decoder
+            decoder_mid_block_types: Types of middle block(s) in the decoder.
+            up_block_types: Type of up block(s) to use for each level.
+            up_layers_per_block: Number of blocks per level in the decoder
+                (blocks are repeated if `>1`).
+            upsample_type: Type of upsampling block
+                (see `chuchichaestli.models.upsampling` for details).
+            block_out_channel_mults: Multiplier for output channels of each level block.
+            use_latent_proj: Whether to use a linear layer between encoder and latent space.
+            use_latent_deproj: Whether to use a linear layer between latent space and decoder.
+            res_act_fn: Activation function for the residual blocks
+                (see `chuchichaestli.models.activations` for details).
+            res_dropout: Dropout rate for the residual blocks.
+            res_norm_type: Normalization type for the residual block
+                (see `chuchichaestli.models.norm` for details).
+            res_groups: Number of groups for the residual block normalization (if group norm).
+            res_kernel_size: Kernel size for the residual blocks.
+            attn_head_dim: Dimension of the attention heads.
+            attn_n_heads: Number of attention heads.
+            attn_dropout_p: Dropout probability of the scaled dot product attention.
+            attn_norm_type: Normalization type for the convolutional attention block
+                (see `chuchichaestli.models.norm` for details).
+            attn_groups: Number of groups for the convolutional attention block normalization
+                (if `attn_norm_type` is `"group"`).
+            attn_kernel_size: Kernel size for the convolutional attention block.
+            encoder_act_fn: Activation function for the output layers in the encoder
+                (see `chuchichaestli.models.activations` for details).
+            encoder_norm_type: Normalization type for the encoder's output block
+                (see `chuchichaestli.models.norm` for details).
+            encoder_groups: Number of groups for normalization in the output layer of the encoder.
+            encoder_kernel_size: Kernel size for the output convolution in the encoder.
+            decoder_act_fn: Activation function for the input/output layers in the decoder
+                (see `chuchichaestli.models.activations` for details).
+            decoder_norm_type: Normalization type for the decoder's output block
+                (see `chuchichaestli.models.norm` for details).
+            decoder_groups: Number of groups for normalization in the input/output layer of the decoder.
+            decoder_kernel_size: Kernel size for the output convolution in the decoder.
         """
         super().__init__()
 
+        self.levels = len(block_out_channel_mults)
+        self.channel_mults = prod(block_out_channel_mults)
+
         if encoder_out_block_type == "DeepCompressionEncoderOutBlock":
-            assert (
-                dimensions == 2
-            ), "DeepCompressionEncoderOutBlock only supports 2D data."
+            assert dimensions == 2, (
+                "DeepCompressionEncoderOutBlock only supports 2D data."
+            )
 
         res_args = {
-            "res_groups": res_groups,
             "res_act_fn": res_act_fn,
             "res_dropout": res_dropout,
+            "res_groups": res_groups,
             "res_norm_type": res_norm_type,
             "res_kernel_size": res_kernel_size,
         }
@@ -110,21 +168,9 @@ class VAE(nn.Module):
         attn_args = {
             "n_heads": attn_n_heads,
             "head_dim": attn_head_dim,
-            "inter_channels": attn_gate_inter_channels,
-        }
-
-        encoder_args = {
-            "groups": encoder_norm_num_groups,
-            "act": encoder_act_fn,
-            "in_kernel_size": encoder_in_kernel_size,
-            "out_kernel_size": encoder_out_kernel_size,
-        }
-
-        decoder_args = {
-            "groups": decoder_norm_num_groups,
-            "act": decoder_act_fn,
-            "in_kernel_size": decoder_in_kernel_size,
-            "out_kernel_size": decoder_out_kernel_size,
+            "dropout_p": attn_dropout_p,
+            "norm_type": attn_norm_type,
+            "groups": attn_groups,
         }
 
         self.encoder = Encoder(
@@ -134,51 +180,59 @@ class VAE(nn.Module):
             out_channels=latent_dim,
             down_block_types=down_block_types,
             block_out_channel_mults=block_out_channel_mults,
-            mid_block_type=mid_block_type,
-            encoder_out_block_type=encoder_out_block_type,
-            num_layers_per_block=num_layers_per_block,
+            num_layers_per_block=down_layers_per_block,
+            mid_block_types=encoder_mid_block_types,
+            out_block_type=encoder_out_block_type,
+            downsample_type=downsample_type,
+            act_fn=encoder_act_fn,
+            norm_type=encoder_norm_type,
+            num_groups=encoder_groups,
+            kernel_size=encoder_kernel_size,
             res_args=res_args,
             attn_args=attn_args,
-            in_out_args=encoder_args,
-            double_z=double_z,
+            double_z=True,
         )
-        self.softplus = nn.Softplus()
         self.decoder = Decoder(
             dimensions=dimensions,
             in_channels=latent_dim,
-            n_channels=n_channels,
-            out_channels=in_channels,
+            n_channels=self.channel_mults * n_channels,
+            out_channels=out_channels,
             up_block_types=up_block_types,
+            in_block_type=decoder_in_block_type,
             block_out_channel_mults=block_out_channel_mults,
-            mid_block_type=mid_block_type,
-            decoder_in_block_type=decoder_in_block_type,
-            num_layers_per_block=num_layers_per_block,
+            num_layers_per_block=up_layers_per_block,
+            mid_block_types=decoder_mid_block_types,
+            upsample_type=upsample_type,
+            act_fn=decoder_act_fn,
+            norm_type=decoder_norm_type,
+            num_groups=decoder_groups,
+            kernel_size=decoder_kernel_size,
             res_args=res_args,
             attn_args=attn_args,
-            in_out_args=decoder_args,
         )
-
-        self.levels = len(block_out_channel_mults)
-
-    def encode(self, x: torch.Tensor, eps=1e-8):
-        """Encode the input.
-
-        Args:
-            x (torch.Tensor): Input tensor.
-            eps (float): Epsilon value. Defaults to 1e-8.
-
-        Returns:
-            torch.distributions.MultivariateNormal: Multivariate normal posterior distribution.
-        """
-        h = self.encoder(x)
-        mean, log_var = h.chunk(2, dim=1)
-        scale = self.softplus(log_var) + eps
-        scale_tril = torch.diag_embed(scale)
-        return torch.distributions.MultivariateNormal(mean, scale_tril=scale_tril)
-
-    def decode(self, z: torch.Tensor):
-        """Decode the input."""
-        return self.decoder(z)
+        self.latent_proj = (
+            DIM_TO_CONV_MAP[dimensions](
+                latent_dim * 2,
+                latent_dim * 2,
+                kernel_size=1,
+                stride=1,
+                padding="same",
+            )
+            if use_latent_proj
+            else None
+        )
+        self.latent_deproj = (
+            DIM_TO_CONV_MAP[dimensions](
+                latent_dim,
+                latent_dim,
+                kernel_size=1,
+                stride=1,
+                padding="same",
+            )
+            if use_latent_deproj
+            else None
+        )
+        self.softplus = nn.Softplus()
 
     def compute_latent_shape(self, input_shape: tuple[int]):
         """Compute the shape of the latent space (no batch dimension)."""
@@ -189,17 +243,44 @@ class VAE(nn.Module):
         )
         return (self.encoder.out_channels, *spatial_dims)
 
-    def forward(self, x: torch.Tensor, eps=1e-8):
+    def encode(self, x: torch.Tensor, eps: float = 1e-12) -> MultivariateNormal:
+        """Encode the input.
+
+        Args:
+            x: Input tensor.
+            eps: Small constant value for numerical stability.
+
+        Returns:
+            Multivariate normal posterior distribution
+        """
+        z = self.encoder(x)
+        z = self.latent_proj(z) if self.latent_proj is not None else z
+        mean, log_var = z.chunk(2, dim=1)
+        scale = self.softplus(log_var) + eps
+        scale_tril = torch.diag_embed(scale)
+        return MultivariateNormal(mean, scale_tril=scale_tril)
+
+    def decode(self, z: torch.Tensor) -> torch.Tensor:
+        """Decode the input."""
+        h = self.latent_deproj(z) if self.latent_deproj is not None else z
+        return self.decoder(h)
+
+    def forward(
+        self, x: torch.Tensor, sample_posterior: bool = True, eps: float = 1e-12
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """Forward pass through the model."""
-        posterior = self.encode(x, eps)
-        z = posterior.rsample()
+        posterior = self.encode(x, eps=eps)
+        if sample_posterior:
+            z = posterior.rsample()
+        else:
+            z = posterior.mode()
         return self.decode(z), posterior
 
     @staticmethod
     def kl_div(posterior: torch.distributions.MultivariateNormal):
-        """Compute the KL divergence."""
+        """Compute the KL divergence between posterior and a multivariate Gaussian."""
         zeros = torch.zeros_like(posterior.mean)
         eye = torch.eye(posterior.mean.shape[-1])
-        return torch.distributions.kl.kl_divergence(
-            posterior, torch.distributions.MultivariateNormal(zeros, eye)
+        return kl.kl_divergence(
+            posterior, MultivariateNormal(zeros, eye)
         )
