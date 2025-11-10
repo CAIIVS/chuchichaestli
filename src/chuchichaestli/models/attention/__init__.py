438fec9c16009e1251892e189e934b5bb1c0f1bf
phdenzel <phdenzel@gmail.com>
2025-11-10 23:02:45 +0100

    Linear multiscale attention block

--- src/chuchichaestli/models/attention/__init__.py
+++ src/chuchichaestli/models/attention/__init__.py
@@ -6,17 +6,19 @@
 from chuchichaestli.models.attention.self_attention import SelfAttention
 from chuchichaestli.models.attention.conv_attention import ConvAttention
 from chuchichaestli.models.attention.attention_gate import AttentionGate
+from chuchichaestli.models.attention.multiscale_attention import LiteMultiscaleAttention
 from typing import Literal
 
 __all__ = ["ATTENTION_MAP"]
 
-AttentionTypes = Literal["self_attention", "conv_attention", "attention_gate"]
-AttentionDownTypes = Literal["self_attention", "conv_attention"]
+AttentionTypes = Literal["self_attention", "conv_attention", "attention_gate", "lite_multiscale_attention"]
+AttentionDownTypes = Literal["self_attention", "conv_attention", "lite_multiscale_attention"]
 
 ATTENTION_MAP = {
     "self_attention": SelfAttention,
     "conv_attention": ConvAttention,
     "attention_gate": AttentionGate,
+    "lite_multiscale_attention": LiteMultiscaleAttention,
 }
 
 
