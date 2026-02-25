8056a02cc6860562f51fb4d8d2811550318e66ed
phdenzel <phdenzel@gmail.com>
2026-02-25 13:48:57 +0100

    refactor: import all dataset classes at data module level

--- src/chuchichaestli/data/__init__.py
+++ src/chuchichaestli/data/__init__.py
@@ -3,7 +3,43 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 """Data module of chuchichaestli."""
 
+from chuchichaestli.data.base import FileDataset, CachingDataset  # abstract
 from chuchichaestli.data.zip import ZipDataset
 from chuchichaestli.data.hdf5 import HDF5Dataset, ZipHDF5Dataset
+from chuchichaestli.data.numpy import NumpyDataset, ZipNumpyDataset
+from chuchichaestli.data.safetensors import SafetensorsDataset, ZipSafetensorsDataset
+from chuchichaestli.data.image import ImageDataset, ZipImageDataset
+from chuchichaestli.data.procedural import (
+    ProceduralDataset, # abstract
+    HalfMoonsDataset,
+    SpiralsDataset,
+    CheckerboardDataset,
+    RingsDataset,
+    ConcentricSpheresDataset,
+    GaussiansDataset,
+    SwissRollDataset,
+    generate_procedural_dataset,
+)
 
-__all__ = ["ZipDataset", "HDF5Dataset", "ZipHDF5Dataset"]
+__all__ = [
+    "FileDataset",
+    "CachingDataset",
+    "ZipDataset",
+    "HDF5Dataset",
+    "ZipHDF5Dataset",
+    "NumpyDataset",
+    "ZipNumpyDataset",
+    "SafetensorsDataset",
+    "ZipSafetensorsDataset",
+    "ImageDataset",
+    "ZipImageDataset",
+    "ProceduralDataset",
+    "HalfMoonsDataset",
+    "SpiralsDataset",
+    "CheckerboardDataset",
+    "RingsDataset",
+    "ConcentricSpheresDataset",
+    "GaussiansDataset",
+    "SwissRollDataset",
+    "generate_procedural_dataset",
+]
